f7b819b98d484ed613a385a302cef927be13d931
tomirodriguez <tomirodriguez.89@gmail.com>
2022-07-21 16:45:26 +0200

    add jest dependencies to work with react

--- client/jest.config.js
+++ client/jest.config.js
@@ -1,5 +1,10 @@
 module.exports = {
-  collectCoverageFrom: ['src/**/*.{ts,tsx}'],
-  coverageDirectory: 'coverage',
+  roots: ['<rootDir>/src'],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   testEnvironment: 'jsdom',
 };
