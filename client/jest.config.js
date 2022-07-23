5f6373679fc5623d7b40f086976ac701ba2f6dae
tomirodriguez <tomirodriguez.89@gmail.com>
2022-07-23 23:50:50 +0200

    libs: change some libs and babel setup to run tests faster

--- client/jest.config.js
+++ client/jest.config.js
@@ -1,8 +1,5 @@
 module.exports = {
   roots: ['<rootDir>/src'],
-  transform: {
-    '^.+\\.tsx?$': 'ts-jest',
-  },
   setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
