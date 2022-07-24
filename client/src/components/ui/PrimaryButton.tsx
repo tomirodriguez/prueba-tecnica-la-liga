69bd13f3a718fbb91e648e824e9b68966b0bcdf3
tomirodriguez <tomirodriguez.89@gmail.com>
2022-07-24 14:53:48 +0200

    ui(PrimaryButton): add hover effect

--- client/src/components/ui/PrimaryButton.tsx
+++ client/src/components/ui/PrimaryButton.tsx
@@ -3,7 +3,13 @@ import { FC } from 'react';
 
 export const PrimaryButton: FC<ButtonProps> = ({ children, ...props }) => {
   return (
-    <Button {...props} bg="secondary.main" color="white">
+    <Button
+      {...props}
+      bg="secondary.main"
+      color="white"
+      _hover={{ bg: 'secondary.dark' }}
+      _focus={{ outline: 'none' }}
+    >
       {children}
     </Button>
   );
