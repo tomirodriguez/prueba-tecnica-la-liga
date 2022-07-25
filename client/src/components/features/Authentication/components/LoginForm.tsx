3b6d51fb4fb0d094e03712c8217b43fe77bbc857
tomirodriguez <tomirodriguez.89@gmail.com>
2022-07-25 17:55:27 +0200

    update(LoginForm): add a title

--- client/src/components/features/Authentication/components/LoginForm.tsx
+++ client/src/components/features/Authentication/components/LoginForm.tsx
@@ -4,7 +4,9 @@ import {
   FormErrorMessage,
   FormHelperText,
   FormLabel,
+  Heading,
   Input,
+  Text,
 } from '@chakra-ui/react';
 import { ChangeEvent, FC, SyntheticEvent, useState } from 'react';
 import { PrimaryButton } from '../../../ui';
@@ -48,48 +50,61 @@ export const LoginForm: FC<Props> = ({
   const hasError = !!errorMessage;
 
   return (
-    <form onSubmit={handleFormSubmit}>
-      <Flex
-        direction={'column'}
-        w={300}
-        shadow={'md'}
-        rounded={'md'}
-        background={`${colorMode}.bg.card`}
-        padding={30}
-        border={'1px'}
-        borderColor={errorMessage ? 'red' : `${colorMode}.bg.card`}
+    <>
+      <Heading as="h1" mb={8}>
+        LaLiga App
+      </Heading>
+      <Text
+        fontSize="2xl"
+        maxW={{ base: 300, xl: 650 }}
+        textAlign="center"
+        mb="10"
       >
-        <FormControl>
-          <FormLabel htmlFor="email">Email</FormLabel>
-          <Input type="email" id="email" onChange={handleEmailChange} />
-        </FormControl>
+        Ingrese sus credenciales para poder entrar a la App
+      </Text>
+      <form onSubmit={handleFormSubmit}>
+        <Flex
+          direction={'column'}
+          w={300}
+          shadow={'md'}
+          rounded={'md'}
+          background={`${colorMode}.bg.card`}
+          padding={30}
+          border={'1px'}
+          borderColor={errorMessage ? 'red' : `${colorMode}.bg.card`}
+        >
+          <FormControl>
+            <FormLabel htmlFor="email">Email</FormLabel>
+            <Input type="email" id="email" onChange={handleEmailChange} />
+          </FormControl>
 
-        <FormControl isInvalid={hasError}>
-          <FormLabel htmlFor="password" mt={5}>
-            Password
-          </FormLabel>
-          <Input
-            aria-errormessage="error-message"
-            type="password"
-            id="password"
-            onChange={handlePasswordChange}
-          />
-          {hasError ? (
-            // Chakra no esta manejando la accesibilidad con mensajes de error. Con esto se soluciona
-            <FormErrorMessage id="error-message">
-              Usuario invalido.
-            </FormErrorMessage>
-          ) : (
-            <FormHelperText textAlign={'right'} fontSize={'xs'}>
-              Nunca compartas tus contraseñas
-            </FormHelperText>
-          )}
-        </FormControl>
+          <FormControl isInvalid={hasError}>
+            <FormLabel htmlFor="password" mt={5}>
+              Password
+            </FormLabel>
+            <Input
+              aria-errormessage="error-message"
+              type="password"
+              id="password"
+              onChange={handlePasswordChange}
+            />
+            {hasError ? (
+              // Chakra no esta manejando la accesibilidad con mensajes de error. Con esto se soluciona
+              <FormErrorMessage id="error-message">
+                Usuario invalido.
+              </FormErrorMessage>
+            ) : (
+              <FormHelperText textAlign={'right'} fontSize={'xs'}>
+                Nunca compartas tus contraseñas
+              </FormHelperText>
+            )}
+          </FormControl>
 
-        <PrimaryButton mt={5} type="submit" isLoading={isLoading}>
-          Entrar
-        </PrimaryButton>
-      </Flex>
-    </form>
+          <PrimaryButton mt={5} type="submit" isLoading={isLoading}>
+            Entrar
+          </PrimaryButton>
+        </Flex>
+      </form>
+    </>
   );
 };
