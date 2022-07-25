e394ccfbd822e0b6a5bc8df3c7c5fcae8101fee0
tomirodriguez <tomirodriguez.89@gmail.com>
2022-07-25 18:17:36 +0200

    update(ThemeToggler): update colors and background

--- client/src/components/features/NavBar/ThemeToggler.tsx
+++ client/src/components/features/NavBar/ThemeToggler.tsx
@@ -1,29 +1,30 @@
 import { MoonIcon, SunIcon } from '@chakra-ui/icons';
-import { Button, Circle, ScaleFade, useColorMode } from '@chakra-ui/react';
+import { Button, ScaleFade, useColorMode } from '@chakra-ui/react';
 import { FC } from 'react';
 
 export const ThemeToggler: FC = () => {
   const { colorMode, toggleColorMode } = useColorMode();
 
   return (
-    <ScaleFade initialScale={1} in whileHover={{ scale: 1.15 }}>
+    <ScaleFade initialScale={1} in whileHover={{ scale: 1.18 }}>
       <Button
+        title="Toggle dark mode"
         shadow={'md'}
         boxShadow="lg"
         p="0"
         onClick={toggleColorMode}
         rounded="full"
         _hover={{
-          background: colorMode === 'dark' ? 'yellow.400' : 'blue.800',
+          background: 'transparent',
         }}
+        _focus={{ background: 'transparent' }}
+        _active={{ background: 'transparent' }}
       >
-        <Circle rounded={'full'} size="35px" bg={'white'}>
-          {colorMode === 'dark' ? (
-            <SunIcon color={'yellow.400'} boxSize="6" />
-          ) : (
-            <MoonIcon color={'blue.800'} boxSize="6" />
-          )}
-        </Circle>
+        {colorMode === 'dark' ? (
+          <SunIcon color={'yellow.400'} boxSize="6" />
+        ) : (
+          <MoonIcon color={'blue.500'} boxSize="6" />
+        )}
       </Button>
     </ScaleFade>
   );
